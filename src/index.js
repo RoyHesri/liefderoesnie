77044b98f114e263881c9510151a45845fa93794
Roy Hesri <royhesri.liefderoesnie@gmail.com>
2025-06-01 15:42:00 +0200

    Gebruik ReactDOM.render vir ouer weergawe van React

---
+++ src/index.js
@@ -0,0 +1,11 @@
+// src/index.js
+import React from "react";
+import ReactDOM from "react-dom";
+import WeddingWebsite from "./WeddingWebsite";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <WeddingWebsite />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
